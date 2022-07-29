80bf9e85422d5ba92b3d78ccda39e1b3fd576a30
Ekaterina Toporkova <toporkovaekaterina5@gmail.com>
2022-07-29 17:49:26 +0300

    Добавлен модуль beautiful_dataframe.py для изменения внешнего вида фрейма.Создана новая колонка с перечислением блюд одной из категорий на отдельных строках.

--- parcing_recipe.py
+++ parcing_recipe.py
@@ -2,21 +2,51 @@ import requests
 from bs4 import BeautifulSoup as bs
 import pandas as pd
 from openpyxl import Workbook
+from itertools import chain
+import numpy as np
+import seaborn as sns
+import beautiful_dataframe
 
 url = 'https://www.russianfood.com/'
-r = requests.get(url)
-soup = bs(r.text, 'html.parser')
+req = requests.get(url)
+soup = bs(req.text, 'html.parser')
+
 recep_list = soup.find_all('dt')
+first_eat_list = soup.find('dd')  # html первых блюд
+first_eat = first_eat_list.text  # строка первых блюд
+first_list = []
+first_list.append(first_eat)
+
 category_list = []  # список категорий блюд
 link_list = []  # список ссылок на категорию блюда
+
 for r in recep_list:
     category_list.append(r.a.text)
     link_list.append('https://www.russianfood.com/' + r.a['href'])
 
-table = pd.DataFrame({'Категория блюд': category_list, 'Ссылка на категорию': link_list})
-print(table)
+
+table = pd.DataFrame({'Категория блюд': category_list, 'Ссылка на категорию': link_list, 'Перечень блюд': '1'})
+table._set_value(0, 'Перечень блюд', *first_list) # новый столбец 'Перечень блюд' со значение в первой строке
+
+
+
+
+splitted = table['Перечень блюд'].str.split(',\s*')
+
+l = splitted.str.len()
+
+
+# новая таблица для записи обновленных данных
+new_table = pd.DataFrame({'Категория блюд': np.repeat(table['Категория блюд'], l),
+                    'Ссылка на категорию': np.repeat(table['Ссылка на категорию'], l),
+                   'Перечень блюд':np.concatenate(splitted.values)}, columns=['Категория блюд','Ссылка на категорию', 'Перечень блюд'])
+
+
+if __name__ == '__main__':
+    print (new_table)
+    print('DataFrame is written successfully to Excel File.')
 
 writer = pd.ExcelWriter('excel_file.xlsx')
-table.to_excel(writer)
+new_table.to_excel(writer)
 writer.save()
-print('DataFrame is written successfully to Excel File.')
+
