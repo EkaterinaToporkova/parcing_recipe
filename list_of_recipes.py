5c74062e323f6510684a570ccba059172fbe730f
Ekaterina Toporkova <toporkovaekaterina5@gmail.com>
2022-08-02 17:25:12 +0300

    Переделан столбец "Ссылки на категорию", теперь на каждою блюдо совя ссылка, добавлен столбец ингридиенты.

--- list_of_recipes.py
+++ list_of_recipes.py
@@ -3,64 +3,90 @@ from bs4 import BeautifulSoup as bs
 
 
 url = 'https://www.russianfood.com/'
-req = requests.get(url)
+header = {'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
+           'upgrade-insecure-requests': '1',
+           'cookie': 'mos_id=CllGxlx+PS20pAxcIuDnAgA=; session-cookie=158b36ec3ea4f5484054ad1fd21407333c874ef0fa4f0c8e34387efd5464a1e9500e2277b0367d71a273e5b46fa0869a; NSC_WBS-QUBG-jo-nptsv-WT-443=ffffffff0951e23245525d5f4f58455e445a4a423660; rheftjdd=rheftjddVal; _ym_uid=1552395093355938562; _ym_d=1552395093; _ym_isad=2'}
+
+req = requests.get(url, headers=header)
+for key, value in req.request.headers.items():
+    print(key+": "+value)
+print(req.status_code)
+
 soup = bs(req.text, 'html.parser')
-
-recep_list = soup.find_all('dt')
-
+recep_list = soup.find_all('dt')  # html категорий блюд
 category_list = []  # список категорий блюд
-link_list = []  # список ссылок на категорию блюда
 for r in recep_list:
     category_list.append(r.a.text)
-    link_list.append('https://www.russianfood.com/' + r.a['href'])
 
-first_eat_list = soup.find('dd')  # html первых блюд
-first_eat = first_eat_list.text  # строка первых блюд
+
+first_eat_list = soup.find_all('a', class_='resList')[1:21] # html первых блюд
+
 first_list = []  # пустой список для первых блюд
-first_list.append(first_eat)  # список первых блюд
+first_link = [] # пустой список ссылок первых блюд
+ing_first = []
+for fl in first_eat_list:
+    first_link.append('https://www.russianfood.com/' + fl['href'])  # список ссылок на первые блюда
+    first_list.append(fl.text)  # список первых блюд
 
 second_eat_list = soup.find_all('a', class_='resList')[22:85]  # html вторых блюд
-
 second_list = []  # список вторых блюд
+second_link = []
 for s in second_eat_list:
+    second_link.append('https://www.russianfood.com/' + s['href'])
     second_list.append(s.text)
 
 zag_eat_list = soup.find_all('a', class_='resList')[86:98]  # html заготовок
 zag_list = []  # список заготовок
+zag_link = []
 for z in zag_eat_list:
+    zag_link.append('https://www.russianfood.com/' + z['href'])
     zag_list.append(z.text)
 
 snacks_eat_list = soup.find_all('a', class_='resList')[99:131]  # html закусок
 snacks_list = []  # список закусок
+snacks_link = []
 for s in snacks_eat_list:
+    snacks_link.append('https://www.russianfood.com/' + s['href'])
     snacks_list.append(s.text)
 
 bakery_eat_list = soup.find_all('a', class_='resList')[132:193]  # html изделий из теста
 bakery_list = []  # список изделий из теста
+bakery_link = []
 for b in bakery_eat_list:
+    bakery_link .append('https://www.russianfood.com/' + b['href'])
     bakery_list.append(b.text)
 
 marinade_eat_list = soup.find_all('a', class_='resList')[194:202]  # html маринада
 marinade_list = []  # список маринада
+marinade_link = []
 for m in marinade_eat_list:
+    marinade_link.append('https://www.russianfood.com/' + m['href'])
     marinade_list.append(m.text)
 
 drink_eat_list = soup.find_all('a', class_='resList')[203:232]  # html напитков
 drink_list = []  # список напитков
+drink_link = []
 for d in drink_eat_list:
+    drink_link.append('https://www.russianfood.com/' + d['href'])
     drink_list.append(d.text)
 
 spice_eat_list = soup.find_all('a', class_='resList')[233:234]  # html приправ
 spice_list = []  # список приправ
+spice_link = []
 for sp in spice_eat_list:
+    spice_link.append('https://www.russianfood.com/' + sp['href'])
     spice_list.append(sp.text)
 
 sweet_eat_list = soup.find_all('a', class_='resList')[235:279]  # html сладостей
 sweet_list = []  # список сладостей
+sweet_link = []
 for sw in sweet_eat_list:
+    sweet_link.append('https://www.russianfood.com/' + sw['href'])
     sweet_list.append(sw.text)
 
 sauces_eat_list = soup.find_all('a', class_='resList')[280:300]  # html соусов
 sauces_list = []  # список соусов
+sauces_link = []
 for sa in sauces_eat_list:
+    sauces_link.append('https://www.russianfood.com/' + sa['href'])
     sauces_list.append(sa.text)
--- parcing_recipe.py
+++ parcing_recipe.py
@@ -8,27 +8,45 @@ import seaborn as sns
 import beautiful_dataframe as bd
 import list_of_recipes as lor
 
+
 # таблица для записи данных
-table = pd.DataFrame({'Категория блюд': lor.category_list, 'Ссылка на категорию': lor.link_list, 'Перечень блюд': '1'})
-table._set_value(0, 'Перечень блюд', *lor.first_list)  # новый столбец 'Перечень блюд' со значениями на каждой строке
+table = pd.DataFrame({'Категория блюд': lor.category_list, 'Перечень блюд': '1', 'Ссылка на категорию': '1', 'Ингредиенты': ''})
+table._set_value(0, 'Перечень блюд', ', '.join(lor.first_list))  # новый столбец 'Перечень блюд' со значениями на каждой строке
+table._set_value(0, 'Ссылка на категорию', ', '.join(lor.first_link))  # перечень ссылок на первый блюда
+table._set_value(0, 'Ингредиенты', ', '.join(lor.first_link))  # перечень ссылок на первый блюда
 table._set_value(1, 'Перечень блюд', ', '.join(lor.second_list))  # добавлен перечень вторых блюд на каждой строке
+table._set_value(1, 'Ссылка на категорию', ', '.join(lor.second_link))
 table._set_value(2, 'Перечень блюд', ', '.join(lor.zag_list))  # добавлен перечень заготовок на каждой строке
+table._set_value(2, 'Ссылка на категорию', ', '.join(lor.zag_link))
 table._set_value(3, 'Перечень блюд', ', '.join(lor.snacks_list))  # добавлен перечень закусок на каждой строке
+table._set_value(3, 'Ссылка на категорию', ', '.join(lor.snacks_link))
 table._set_value(4, 'Перечень блюд', ', '.join(lor.bakery_list))  # добавлен перечень изделий из теста на каждой строке
+table._set_value(4, 'Ссылка на категорию', ', '.join(lor.bakery_link))
 table._set_value(5, 'Перечень блюд', ', '.join(lor.marinade_list))  # добавлен перечень маринада на каждой строке
+table._set_value(5, 'Ссылка на категорию', ', '.join(lor.marinade_link))
 table._set_value(6, 'Перечень блюд', ', '.join(lor.drink_list))  # добавлен перечень напитков на каждой строке
+table._set_value(6, 'Ссылка на категорию', ', '.join(lor.drink_link))
 table._set_value(7, 'Перечень блюд', ', '.join(lor.spice_list))  # добавлен перечень приправ на каждой строке
+table._set_value(7, 'Ссылка на категорию', ', '.join(lor.spice_link))
 table._set_value(8, 'Перечень блюд', ', '.join(lor.sweet_list))  # добавлен перечень сладостей на каждой строке
+table._set_value(8, 'Ссылка на категорию', ', '.join(lor.sweet_link))
 table._set_value(9, 'Перечень блюд', ', '.join(lor.sauces_list))  # добавлен перечень соусов на каждой строке
+table._set_value(9, 'Ссылка на категорию', ', '.join(lor.sauces_link))
 
 splitted = table['Перечень блюд'].str.split(',\s*')
+
 l = splitted.str.len()
+
+splitted_link = table['Ссылка на категорию'].str.split(',\s*')
+
+
 
 # новая таблица для записи обновленных данных
 new_table = pd.DataFrame({'Категория блюд': np.repeat(table['Категория блюд'], l),
-                          'Ссылка на категорию': np.repeat(table['Ссылка на категорию'], l),
-                          'Перечень блюд': np.concatenate(splitted.values)},
-                         columns=['Категория блюд', 'Ссылка на категорию', 'Перечень блюд'])
+                          'Перечень блюд': np.concatenate(splitted.values),
+                          'Ссылка на категорию': np.concatenate(splitted_link.values),
+                          'Ингредиенты': np.repeat(table['Ингредиенты'], l)},
+                         columns=['Категория блюд', 'Перечень блюд', 'Ссылка на категорию', 'Ингредиенты'])
 
 if __name__ == '__main__':
     print(new_table)
